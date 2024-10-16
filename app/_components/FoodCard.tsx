81ec069766eae18a1fe7006d40f497802a99046c
nojiritakeshi <takesoccer08nine@ezweb.ne.jp>
2024-10-16 23:40:26 +0900

    add suspence

--- app/_components/FoodCard.tsx
+++ app/_components/FoodCard.tsx
@@ -1,23 +1,45 @@
 'use client';
-import React from 'react';
+import React, { cache } from 'react';
 import Image from 'next/image';
 import NextLink from 'next/link';
 import { Klee_One } from 'next/font/google';
-import { FoodJSON } from '../food/page';
 import config from '../../next.config.mjs';
+import { useSearchParams } from 'next/navigation';
 
 const BASE_PATH = config.basePath ? config.basePath : '';
 
 const kleeOne = Klee_One({ weight: '400', subsets: ['latin'] });
 
+export type FoodJSON = {
+  [key: string]: {
+    path: string;
+    name: string;
+    subName: string;
+    genre: string;
+    description: string;
+  };
+};
+
+const getData = async (): Promise<FoodJSON> => {
+  const data = await fetch('/food.json', {
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    }
+  }).then(function (response) {
+    return response.json() as Promise<FoodJSON>;
+  });
+  return data;
+};
+
 // foodを引数にとる
-export const FoodCard: React.FC<FoodJSON[string]> = ({
-  path,
-  name,
-  subName,
-  genre,
-  description
-}) => {
+export const FoodCard: React.FC = cache(async () => {
+  // URLを取得
+  // 頭のスラッシュを削除
+  const searchParams = useSearchParams();
+  const foodPath = searchParams.get('genre') || '';
+  const json = await getData();
+  const food: FoodJSON[string] = json[foodPath];
   return (
     <div className={`rounded-sm max-w-lg mx-auto ${kleeOne.className}`}>
       {/* 戻るボタン */}
@@ -26,17 +48,17 @@ export const FoodCard: React.FC<FoodJSON[string]> = ({
       </NextLink>
       <div className="mt-2 mb-2">
         <span className="bg-black text-white text-sm p-1 rounded-md">
-          {genre}
+          {food.genre}
         </span>
       </div>
       {/* サブタイトルみたいな文言 ~を添えてみたいな */}
-      <p className="text-sm">{subName}</p>
+      <p className="text-sm">{food.subName}</p>
       {/* タイトル */}
-      <h2 className="text-2xl font-bold">{name}</h2>
+      <h2 className="text-2xl font-bold">{food.name}</h2>
       {/* メイン画像 */}
       <div className="mt-4 relative w-full h-48">
         <Image
-          src={`${BASE_PATH}/${path}.jpg`} // ここに /public フォルダ内の画像ファイル名を記載
+          src={`${BASE_PATH}/${food.path}.jpg`} // ここに /public フォルダ内の画像ファイル名を記載
           alt="courseの画像"
           layout="fill"
           objectFit="cover"
@@ -45,7 +67,7 @@ export const FoodCard: React.FC<FoodJSON[string]> = ({
       </div>
 
       {/* 説明文 */}
-      <p className="text-sm mt-4">{description}</p>
+      <p className="text-sm mt-4">{food.description}</p>
     </div>
   );
-};
+});
--- app/food/page.tsx
+++ app/food/page.tsx
@@ -1,37 +1,12 @@
-'use client';
-import { useSearchParams } from 'next/navigation';
-import { cache } from 'react';
+import { Suspense } from 'react';
 import { FoodCard } from '../_components/FoodCard';
 
-export type FoodJSON = {
-  [key: string]: {
-    path: string;
-    name: string;
-    subName: string;
-    genre: string;
-    description: string;
-  };
-};
-
-const getData = async (): Promise<FoodJSON> => {
-  const data = await fetch('/food.json', {
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json'
-    }
-  }).then(function (response) {
-    return response.json() as Promise<FoodJSON>;
-  });
-  return data;
-};
-
-export default cache(async function Page() {
-  // URLを取得
-  // 頭のスラッシュを削除
-  const searchParams = useSearchParams();
-  const foodPath = searchParams.get('genre') || '';
-  const json = await getData();
-  const food: FoodJSON[string] = json[foodPath];
-  console.log(food);
-  return <FoodCard {...food} />;
-});
+export default function Page() {
+  return (
+    <>
+      <Suspense fallback={<div>Loading...</div>}>
+        <FoodCard />
+      </Suspense>
+    </>
+  );
+}
